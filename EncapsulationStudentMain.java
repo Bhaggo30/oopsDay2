6fd1dd09c83ab2d482df4df3b323d7cfe328990b
bhagyashree <bhaggo30july@gmail.com>
2024-06-26 13:13:27 +0530

    main class

--- EncapsulationStudentMain.java
+++ EncapsulationStudentMain.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class EncapsulationStudentMain {
+import javax.lang.model.util.SimpleElementVisitor14;
+
+public class EncapsulationStudentMain {
+
+    public static void main(String[] args) {
+
+        EncapsulationStudent student =new EncapsulationStudent();
+        student.setsId(101);
+        student.setsName("bhagyashree");
+        student.setsAddress("banglore");
+
+        int id = student.getsId();
+        String name = student.getsName();
+        String address = student.getsAddress();
+
+        System.out.println("Id of the student  "+student.getsId());
+        System.out.println("name of the student  "+name);
+        System.out.println("address of the student "+address);
+
+    }
 }
