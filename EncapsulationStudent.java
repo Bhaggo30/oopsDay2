6d7edc30af66efc480f19f0e7873bbbb28402512
bhagyashree <bhaggo30july@gmail.com>
2024-06-26 13:09:37 +0530

    program for encapsulation

---
+++ EncapsulationStudent.java
@@ -0,0 +1,39 @@
+public class EncapsulationStudent {
+
+    private  int sId;
+    private String sName;
+    private String sAddress;
+
+    public int getsId() {
+        return sId;
+    }
+
+    public void setsId(int sId) {
+        this.sId = sId;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public String getsAddress() {
+        return sAddress;
+    }
+
+    public void setsAddress(String sAddress) {
+        this.sAddress = sAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Encapsulation{" +
+                "sId=" + sId +
+                ", sName='" + sName + '\'' +
+                ", sAddress='" + sAddress + '\'' +
+                '}';
+    }
+}
